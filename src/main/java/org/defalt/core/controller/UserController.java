2454d400c5f9dc88009b3c062256de0e7bf2b3c1
adnan <a.aliyari@gmail.com>
2024-03-02 02:54:51 +0330

    optimised UserController imports

--- src/main/java/org/defalt/core/controller/UserController.java
+++ src/main/java/org/defalt/core/controller/UserController.java
@@ -1,19 +1,18 @@
 package org.defalt.core.controller;
 
 import lombok.AllArgsConstructor;
-import org.defalt.core.context.auth.UserSecurityContext;
 import org.defalt.core.controller.abstracts.AbstractEntityDTOController;
 import org.defalt.core.entity.User;
-import org.defalt.core.model.entity.post.PostPublicationListingDTO;
 import org.defalt.core.model.entity.user.UserCreationDTO;
 import org.defalt.core.model.entity.user.UserFullDTO;
 import org.defalt.core.model.entity.user.UserUpdateDTO;
 import org.defalt.core.repository.AbstractEntityRepository;
 import org.defalt.core.service.AbstractEntityService;
-import org.defalt.core.service.PostPublicationService;
 import org.defalt.core.service.UserService;
-import org.keycloak.representations.idm.UserRepresentation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.function.Supplier;
 
